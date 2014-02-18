eca71d6bb465cc4a98e91276a26c5de10d15bd6f
lauren mccarthy <laurmccarthy@gmail.com>
2014-02-18 08:57:52 -0500

    improvements to fs

--- controller.js
+++ controller.js
@@ -2,10 +2,10 @@
 var _ = require('underscore')
   , twitter = require('ntwitter')
   , request = require('request')
-  , fs = require('fs')
+  , fs = require('fs-extra')
   , beagle = require('beagle')
-  , natural = require("natural")
-  , similarity = require("./similarity")
+  , natural = require('natural')
+  , similarity = require('./similarity')
   , vine = require('./vidgrab');
 
 
@@ -41,8 +41,9 @@ module.exports = function(config) {
   // starts system with queued up next_user
   controller.start = function() {
 
-    controller.reset();
-
+    if (controller.cur_user && controller.cur_user != controller.next_user) {
+      fs.remove('./assets/'+controller.cur_user+'/');
+    }
     controller.cur_user = controller.next_user;
     console.log('start with user '+controller.cur_user);
     //controller.next_user = null;
@@ -57,73 +58,66 @@ module.exports = function(config) {
           }); 
         }
 
-        // download media
-        for (var i=0; i<data.length; i++) {
-          var media = data[i].entities.media;
-          if (media) {
-            for (var j=0; j<media.length; j++) {
-              download(media[j].media_url, function(){console.log('downloaded ');});
-            }
-          }
-          var urls = data[i].entities.urls;
-          if (urls) {
-            for (var j=0; j<urls.length; j++) {
-              if (urls[j].expanded_url.indexOf('vine') !== -1) {
-                var vine_id = urls[j].expanded_url.substring(urls[j].expanded_url.lastIndexOf('/')+1);
-                vine.download(vine_id, {dir: './assets/'});
-              } else {
-                scrape(urls[j].expanded_url, function(){console.log('scraped ');});
-              }
-            }
-          }
-          // console.log(data[i].entities.urls);
-          // console.log(data[i].entities.media);
-        }
+        downloadMedia(data, './assets/'+controller.cur_user+'/');
+
         // analyze tweets
         data = concat_tweets(data);
         findMentor(controller.cur_user, data);
       });
   };
 
-  controller.reset = function() {
-    // empty assets directory
-    var dirPath = 'assets/'
-    fs.readdir(dirPath, function(err, files) {
-      _.each(files, function(file) {
-        if (file !== '.gitkeep') {
-          var filePath = dirPath + file;
-          fs.stat(filePath, function(err, stats) {
-            if (err) console.log(JSON.stringify(err));
-            if (stats.isFile()) {
-              fs.unlink(filePath, function(err) {
-                if (err) console.log(JSON.stringify(err));
-              });
-            }
-          });
-        }
-      });
-    });
-  };
-
   controller.buildDB = function() {
 
-    fs.readFile('./data/mentors.json', 'utf8', function(err, data) {
-      if (data) data = JSON.parse(data);
-      data.forEach(function(mentor) {
-        console.log('building tweets for '+mentor.user);
-        twit.getUserTimeline({screen_name:mentor.user},
-          function(err, data) { 
-            if (err) console.log(err); 
-            data = concat_tweets(data);
-            // insert into db
-            console.log('inserting '+mentor.user);
-            mentor.text = data;
-            controller.storage.insert(mentor);
+    fs.remove('./assets/mentors/', function() {
+      fs.readJson('./data/mentors.json', function(err, data) {
+        data.forEach(function(mentor) {
+          console.log('building tweets for '+mentor.user);
+          twit.getUserTimeline({screen_name:mentor.user},
+            function(err, data) { 
+              if (err) console.log(err); 
+
+              downloadMedia(data, './assets/mentors/'+mentor.user+'/');
+
+              data = concat_tweets(data);
+              // insert into db
+              console.log('inserting '+mentor.user);
+              mentor.text = data;
+              controller.storage.insert(mentor);
+          });
         });
       });
     });
-
   };
+
+  function downloadMedia(data, dir) {
+    fs.mkdirs(dir, function (err) {
+      if (err) console.error(err)
+      else console.log('pow!')
+
+       // download media
+      for (var i=0; i<data.length; i++) {
+        var media = data[i].entities.media;
+        if (media) {
+          for (var j=0; j<media.length; j++) {
+            download(media[j].media_url, dir, function(){console.log('downloaded ');});
+          }
+        }
+        var urls = data[i].entities.urls;
+        if (urls) {
+          for (var j=0; j<urls.length; j++) {
+            if (urls[j].expanded_url.indexOf('vine') !== -1) {
+              var vine_id = urls[j].expanded_url.substring(urls[j].expanded_url.lastIndexOf('/')+1);
+              vine.download(vine_id, {dir: dir});
+            } else {
+              scrape(urls[j].expanded_url, dir, function(){console.log('scraped ');});
+            }
+          }
+        }
+      }
+        // console.log(data[i].entities.urls);
+        // console.log(data[i].entities.media);
+    });
+  }
 
   function findMentor(user, text, save) {
 
@@ -178,18 +172,18 @@ module.exports = function(config) {
     }
   }
 
-  function scrape(uri, callback) {
+  function scrape(uri, dir, callback) {
     beagle.scrape(uri, function(err, bone){
       for (var i=0; i<bone.images.length; i++) {
-        download(bone.images[i], function(){console.log('d');});
+        download(bone.images[i], dir, function(){console.log('d');});
       }
     });
   }
 
-  function download(uri, callback){
+  function download(uri, dir, callback){
     request.head(uri, function(err, res, body){
       var time = new Date().getTime();
-      var filename = 'assets/'+time+'.png';
+      var filename = dir+time+'.png';
       request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
   }
