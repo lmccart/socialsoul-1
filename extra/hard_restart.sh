0aa0958590bc9f5d59cdf57191ce510eb3d14767
Kyle McDonald <kyle@kylemcdonald.net>
2014-03-16 09:25:05 -0700

    start screens after node app

--- extra/hard_restart.sh
+++ extra/hard_restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /Users/socialsoulserver/socialsoul
-./extra/start_all.exp
 /usr/local/bin/forever stopall
 /usr/local/bin/forever start -o logs/out.log -e logs/err.log --spinSleepTime 1000 --minUptime 5000 app.js
+sleep 10
+./extra/start_all.exp
