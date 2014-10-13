d9c3fe5accb7f7122174a49949575d2b8c0e7b51
Michael Reed <mhr2126@gmail.com>
2014-10-13 11:47:19 -0400

    standardize hello message from exit.html

--- public/javascripts/exit.js
+++ public/javascripts/exit.js
@@ -9,7 +9,7 @@ window.onload = function() {
   var host = 'http://' + window.location.hostname + ':3030';
   window.socket = io.connect(host); 
   
-  socket.emit('send', { message: 'hello from frontend screen ' + window.location.toString() });
+  socket.emit('send', { message: 'hello from frontend: ' + window.location.toString() });
 
   socket.on('sync', function (data) {
     console.log(data);
