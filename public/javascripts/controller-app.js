8136808439521b82468f1982ea1b5714a3b84dc1
Michael Reed <mhr2126@gmail.com>
2014-10-13 10:47:45 -0400

    add url to hello messages

--- public/javascripts/controller-app.js
+++ public/javascripts/controller-app.js
@@ -23,7 +23,7 @@ window.onload = function() {
   var host = 'http://' + window.location.hostname + ':3030';
   window.socket = io.connect(host); 
   
-  socket.emit('send', { message: 'hello from frontend' });
+  socket.emit('send', { message: 'hello from frontend: ' + window.location.toString() });
   socket.emit('controller', { action: 'update' });
 
 
