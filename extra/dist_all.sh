722d2693870a962539a3c15307281a636c022f51
Michael Reed <mhr2126@gmail.com>
2014-10-13 11:25:45 -0400

    handle Desktop properly in dist_all.sh

--- extra/dist_all.sh
+++ extra/dist_all.sh
@@ -8,11 +8,12 @@ LOGIN="socialsoulserver@$SERVER"
 ssh -o "StrictHostKeyChecking no" $LOGIN "cd /socialsoul && git fetch && git reset --hard origin/master"
 ssh -o "StrictHostKeyChecking no" $LOGIN < systemsettings.sh
 ssh -o "StrictHostKeyChecking no" $LOGIN "sudo launchctl unload -w /Library/LaunchDaemons || true"
-ssh -o "StrictHostKeyChecking no" $LOGIN "sudo rm -rf /Library/LaunchDaemons"
+ssh -o "StrictHostKeyChecking no" $LOGIN "sudo rm -rf /Library/LaunchDaemons /Users/socialsoulserver/Desktop"
 rsync --delete -r -e ssh LaunchDaemons.server "$LOGIN:/tmp/"
+rsync --delete -r -e ssh Desktop.server "$LOGIN:/tmp/"
 ssh -o "StrictHostKeyChecking no" $LOGIN "sudo mv /tmp/LaunchDaemons.server /Library/LaunchDaemons"
 ssh -o "StrictHostKeyChecking no" $LOGIN "sudo chown -R root /Library/LaunchDaemons"
-rsync --delete -r -e ssh Desktop.server "$LOGIN:/Users/socialsoulserver/Desktop"
+ssh -o "StrictHostKeyChecking no" $LOGIN "mv /tmp/Desktop.server /Users/socialsoulserver/Desktop"
 ssh -o "StrictHostKeyChecking no" $LOGIN "sudo launchctl load -w /Library/LaunchDaemons"
 
 # Client setup
@@ -21,10 +22,11 @@ for i in {0..4} ; do
 	LOGIN="socialsoul@$SERVER"
 	ssh -o "StrictHostKeyChecking no" $LOGIN < systemsettings.sh
 	ssh -o "StrictHostKeyChecking no" $LOGIN "sudo launchctl unload -w /Library/LaunchDaemons || true"
-	ssh -o "StrictHostKeyChecking no" $LOGIN "sudo rm -rf /Library/LaunchDaemons"
+	ssh -o "StrictHostKeyChecking no" $LOGIN "sudo rm -rf /Library/LaunchDaemons /Users/socialsoul/Desktop"
 	rsync --delete -r -e ssh LaunchDaemons.screen "$LOGIN:/tmp/"
+	rsync --delete -r -e ssh Desktop.screen "$LOGIN:/tmp/"
 	ssh -o "StrictHostKeyChecking no" $LOGIN "sudo mv /tmp/LaunchDaemons.screen /Library/LaunchDaemons"
 	ssh -o "StrictHostKeyChecking no" $LOGIN "sudo chown -R root /Library/LaunchDaemons"
-	rsync --delete -r -e ssh Desktop.screen "$LOGIN:/Users/socialsoul/Desktop"
+	ssh -o "StrictHostKeyChecking no" $LOGIN "mv /tmp/Desktop.screen /Users/socialsoul/Desktop"
 	ssh -o "StrictHostKeyChecking no" $LOGIN "sudo launchctl load -w /Library/LaunchDaemons"
 done
