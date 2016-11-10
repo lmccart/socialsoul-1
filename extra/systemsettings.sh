9479f2a45bd12b85400325b28fa46654208bfb7c
socialsoul <socialsoul@socialsoulserver.local>
2016-11-10 22:25:57 +0200

    disable safari popups

--- extra/systemsettings.sh
+++ extra/systemsettings.sh
@@ -15,3 +15,8 @@ sudo pmset repeat wakeorpoweron MTWRFSU "09:00:00" shutdown MTWRFSU "23:00:00"
 # configure volume
 osascript -e 'set volume alert volume 0'
 osascript -e 'set volume output volume 100'
+
+# disable safari popups
+defaults write com.apple.coreservices.uiagent CSUIHasSafariBeenLaunched -bool YES
+defaults write com.apple.coreservices.uiagent CSUIRecommendSafariNextNotificationDate -date 2050-01-01T00:00:00Z
+defaults write com.apple.coreservices.uiagent CSUILastOSVersionWhereSafariRecommendationWasMade -float 10.99
